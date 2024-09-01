c5d88cb6729bfcbb26f049ddeea0377584af1c66
VictorGuui <victorguilhermebranco@outlook.com>
2024-08-31 21:53:05 -0300

    feat: Add routes

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+
+import appDataSource from "../infra/data-source";
+import { User } from "../infra/entities/user";
+import { UserService } from "../services";
+import { UserController } from "../controller";
+
+const userRouter = Router()
+
+const service = new UserService(appDataSource.getRepository(User))
+const controller = new UserController(service)
+
+userRouter.post('/create', async (req, res) => {
+    await controller.createUserController(req, res)
+})
+
+userRouter.post('/login', async (req, res) => {
+    await controller.loginController(req, res)
+})
+
+export default userRouter
