965205587746862ec94f7011f5d9ca1e9bfb068c
VictorGuui <victorguilhermebranco@outlook.com>
2024-08-31 21:50:47 -0300

    feat: Update connection

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import Express, { Request, Response } from 'express';
-import { router } from './routes/router';
+import cors from "cors";
+import userRouter from './routes/user.routes';
+import appDataSource from './infra/data-source'
 
 const app = Express()
 require('dotenv').config()
 
-app.listen(process.env.PORT, () => {
-  console.log(`Ta rodando nessa porta aqui tá ✌️  http://localhost:${process.env.PORT}`)
+app.use(Express.json())
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}))
+
+appDataSource.initialize().then((connection) => {
+  console.log("Banco de dados inicializado")
+  app.listen(process.env.PORT, () => {
+      console.log(`Ta rodando nessa porta aqui tá ✌️:  http://localhost:${process.env.PORT}`)
+  })
 })
 
 // Rotas onde iremos fazer as req e res
@@ -13,4 +24,4 @@ app.get('/', (req, res) => {
   res.send('Olá Kersys')
 })
 
-app.use('/router', router)
+app.use('/user', userRouter)
